9051d4930fe6d8cbd9580acf7dfa39072d4a5180
Kai Liao <38992157+yifanyangl@users.noreply.github.com>
2018-12-17 19:50:18 -0800

    easy 001 4ms 100%
    To many lines, can improve

--- algorithms/easy_001_two_sums.go
+++ algorithms/easy_001_two_sums.go
@@ -1,13 +1,68 @@
 package algorithms
 
+import "sort"
+
 func twoSum(nums []int, target int) []int {
-	for i, v := range nums {
-		vTarget := target - v
-		for iNext, vNext := range nums[i+1:] {
-			if vTarget == vNext {
-				return []int{i, iNext + i + 1}
+	numSorted := make([]int, len(nums))
+	for i, num := range nums {
+		numSorted[i] = num
+	}
+	sort.Ints(numSorted)
+
+	numFirst, numSec := 0, 0
+	found := false
+	for i, num := range numSorted {
+		if num < 0 {
+			if numSorted[len(numSorted)-1] > target {
+				numTarget := target - num
+				for iNext := len(numSorted) - 1; iNext >= 0; iNext-- {
+					numNext := numSorted[iNext]
+					if numNext < numTarget {
+						break
+					} else if numNext == numTarget {
+						found = true
+						numFirst = num
+						numSec = numNext
+						break
+					}
+				}
+			}
+		} else {
+			numTarget := target - num
+			for _, numNext := range numSorted[i+1:] {
+				if numNext > numTarget {
+					break
+				} else if numNext == numTarget {
+					found = true
+					numFirst = num
+					numSec = numNext
+					break
+				}
 			}
 		}
+
+		if found {
+			break
+		}
 	}
-	return nil
+
+	iFirst, iSec := -1, -1
+	for i, num := range nums {
+		if iFirst == -1 && num == numFirst {
+			iFirst = i
+			continue
+		}
+		if iSec == -1 && num == numSec {
+			iSec = i
+			continue
+		}
+		if iFirst != -1 && iSec != -1 {
+			break
+		}
+	}
+	if iFirst > iSec {
+		iFirst, iSec = iSec, iFirst
+	}
+
+	return []int{iFirst, iSec}
 }
--- algorithms/easy_001_two_sums_test.go
+++ algorithms/easy_001_two_sums_test.go
@@ -12,6 +12,8 @@ func TestTwoSums(t *testing.T) {
 		want     []int
 	}{
 		{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
+		{[]int{2, -2, 7, 11, 15}, 9, []int{1, 3}},
+		{[]int{3, 3}, 6, []int{0, 1}},
 	}
 	for _, c := range cases {
 		got := twoSum(c.inNums, c.inTarget)
